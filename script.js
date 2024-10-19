c103a6e502080768e96622427da773770b786802
KS25KS <kanavjeetsingh25@gmail.com>
2024-10-20 00:15:50 +0200

    created nav-bar

---
+++ script.js
@@ -0,0 +1,10 @@
+const links = document.querySelectorAll('.nav-link');
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        // Remove 'active' class from all links
+        links.forEach(l => l.classList.remove('active'));
+        // Add 'active' class to the clicked link
+        this.classList.add('active');
+    });
+});
